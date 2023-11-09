c647314c0fb3b8f9358248a4b1ab3ce232949d37
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2023-11-08 23:34:07 -0300

    estilos do formCadastro

--- frontend/src/components/FormCadastro/index.jsx
+++ frontend/src/components/FormCadastro/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Importe o useHistory
+import { useNavigate } from "react-router-dom";
 import api from "../../api/index";
 import style from "./style.module.css";
 
@@ -18,8 +18,8 @@ function FormCadastro() {
         neighborhood: "",
         complement: "",
     });
-    const [sucesso, setSucesso] = useState(false); // Novo estado para o sucesso
-    const navegate = useNavigate(); // Inicialize o useHistory
+    const [sucesso, setSucesso] = useState(false);
+    const navigate = useNavigate();
 
     const handleTipoUsuarioChange = (e) => {
         setTipoUsuario(e.target.value);
@@ -47,11 +47,11 @@ function FormCadastro() {
             const response = await api.post('/users', novoUsuario);
 
             if (response.status === 201) {
-                setSucesso(true); // Define o estado de sucesso como verdadeiro
-                // Redireciona para outra página após o sucesso
+                setSucesso(true);
+
                 setTimeout(() => {
-                    navegate.push("/loginvendedor");
-                }, 3000); // Redireciona após 3 segundos (você pode ajustar esse valor)
+                    navigate("/loginvendedor");
+                }, 3000);
             }
         } catch (error) {
             console.error('Erro ao criar usuário:', error);
@@ -59,134 +59,152 @@ function FormCadastro() {
     };
 
     return (
-        <div className={`${style.container} ${containerClassName}`}>
-            <div className={style.containertipo}>
-                <div className={`${style.tipoUsuario} ${tipoUsuarioClassName} `}>
-                    <h1>Como deseja se cadastrar ?</h1>
-                    <select value={tipoUsuario} onChange={handleTipoUsuarioChange}>
-                        <option value="1">Produtor</option>
-                        <option value="3">Comprador</option>
-                    </select>
-                </div>
-            </div>
+        <div>
+            {!sucesso && (
+                <div className={`${style.container} ${containerClassName}`}>
+                    <div className={style.containertipo}>
+                        <div className={`${style.tipoUsuario} ${tipoUsuarioClassName} `}>
+                            <h1>Como deseja se cadastrar ?</h1>
+                            <select value={tipoUsuario} onChange={handleTipoUsuarioChange}>
+                                <option value="1">Produtor</option>
+                                <option value="2">Comprador</option>
+                            </select>
+                        </div>
+                    </div>
+                    {tipoUsuario == "1" ? (<form className={style.form} onSubmit={handleCadastroSubmit} method="post" action="/users">
+                        <div className={style.formLeft}>
+                            <label htmlFor="" className={style.labelForm}>
+                                Informações de Cadastro (Produtor)
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Primeiro Nome"
+                                value={primeiroNome}
+                                onChange={(e) => setPrimeiroNome(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Sobrenome"
+                                value={sobrenome}
+                                onChange={(e) => setSobrenome(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="password"
+                                placeholder="Senha"
+                                value={senha}
+                                onChange={(e) => setSenha(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={style.formRight}>
+                            <label htmlFor="" className={style.labelForm}>
+                                Informações de Contato (Produtor)
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Fazenda"
+                                value={fazenda}
+                                onChange={(e) => setFazenda(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Telefone"
+                                value={telefone}
+                                onChange={(e) => setTelefone(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Cidade"
+                                value={endereco.city}
+                                onChange={(e) => setEndereco({ ...endereco, city: e.target.value })}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Bairro"
+                                value={endereco.neighborhood}
+                                onChange={(e) => setEndereco({ ...endereco, neighborhood: e.target.value })}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Rua"
+                                value={endereco.road}
+                                onChange={(e) => setEndereco({ ...endereco, road: e.target.value })}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Número"
+                                value={endereco.number}
+                                onChange={(e) => setEndereco({ ...endereco, number: e.target.value })}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Complemento (Opcional)"
+                                value={endereco.complement}
+                                onChange={(e) => setEndereco({ ...endereco, complement: e.target.value })}
+                            />
+                            <button type="submit">Cadastrar</button>
+                        </div>
+                    </form>
+                    ) : (<form className={style.form} onSubmit={handleCadastroSubmit} method="post">
+                        <div className={style.formLeft}>
+                            <label htmlFor="" className={style.labelForm}>
+                                Informações de Cadastro (Produtor)
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Primeiro Nome"
+                                value={primeiroNome}
+                                onChange={(e) => setPrimeiroNome(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Sobrenome"
+                                value={sobrenome}
+                                onChange={(e) => setSobrenome(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="password"
+                                placeholder="Senha"
+                                value={senha}
+                                onChange={(e) => setSenha(e.target.value)}
+                                required
+                            />
 
-            {sucesso ? ( // Renderiza a animação de sucesso quando sucesso for verdadeiro
-                <div className={style.animacaoSucesso}>
-                    <p>Cadastro realizado com sucesso!</p>
+                            <button type="submit">Cadastrar</button>
+
+                        </div>
+                    </form>)}
                 </div>
-            ) : tipoUsuario === "1" ? (
-                <form className={style.form} onSubmit={handleCadastroSubmit} method="post" action="/users">
-                    <div className={style.formLeft}>
-                        <label htmlFor="" className={style.labelForm}>
-                            Informações de Cadastro (Produtor)
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Primeiro Nome"
-                            value={primeiroNome}
-                            onChange={(e) => setPrimeiroNome(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Sobrenome"
-                            value={sobrenome}
-                            onChange={(e) => setSobrenome(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Senha"
-                            value={senha}
-                            onChange={(e) => setSenha(e.target.value)}
-                        />
-                    </div>
-                    <div className={style.formRight}>
-                        <label htmlFor="" className={style.labelForm}>
-                            Informações de Contato (Produtor)
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Fazenda"
-                            value={fazenda}
-                            onChange={(e) => setFazenda(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Telefone"
-                            value={telefone}
-                            onChange={(e) => setTelefone(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Cidade"
-                            value={endereco.city}
-                            onChange={(e) => setEndereco({ ...endereco, city: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Bairro"
-                            value={endereco.neighborhood}
-                            onChange={(e) => setEndereco({ ...endereco, neighborhood: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Rua"
-                            value={endereco.road}
-                            onChange={(e) => setEndereco({ ...endereco, road: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Número"
-                            value={endereco.number}
-                            onChange={(e) => setEndereco({ ...endereco, number: e.target.value })}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Complemento (Opcional)"
-                            value={endereco.complement}
-                            onChange={(e) => setEndereco({ ...endereco, complement: e.target.value })}
-                        />
-                        <button type="submit">Cadastrar</button>
-                    </div>
-                </form>
-            ) : tipoUsuario === "3" ? (
-                <form className={style.form}>
-                    <div className={style.formLeft}>
-                        <label htmlFor="" className={style.labelForm}>
-                            Informações de Cadastro (Comprador)
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Primeiro Nome"
-                            value={primeiroNome}
-                            onChange={(e) => setPrimeiroNome(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Sobrenome"
-                            value={sobrenome}
-                            onChange={(e) => setSobrenome(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Senha"
-                            value={senha}
-                            onChange={(e) => setSenha(e.target.value)}
-                        />
-                        <button type="submit">Cadastrar</button>
-                    </div>
-                </form>
+            )}
+
+            {sucesso ? (
+                <div className={style.loadingContainer}>
+                    <div className={`${style.loading} ${style.greenLoading}`}></div>
+                </div>
             ) : null}
         </div>
     );
